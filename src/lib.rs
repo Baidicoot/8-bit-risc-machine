c1b4f8870bed9850fc342675f9e9af4dac2b7cf8
Aidan Ewart <aidanprattewart@icloud.com>
2019-01-16 19:12:05 +0000

    implemented conversion from instruction code to instruction

--- src/lib.rs
+++ src/lib.rs
@@ -10,14 +10,15 @@ pub struct Machine {
     registers: [u8; REGISTERS],
     prgcount: u8, //index on disc
     dsccount: u8, //current disc
+    isactive: bool, //is it on or not?
 }
 
-fn run(program: Vec<String>) {
-    let mut vm = Machine { mem: [[0; 256]; DISCS], registers: [0; REGISTERS], prgcount: 0, dsccount: 0, };
+fn run(program: Vec<String>) ->Result<(), &'static str> {
+    let mut vm = Machine { mem: [[0; 256]; DISCS], registers: [0; REGISTERS], prgcount: 0, dsccount: 0, isactive: true, };
     fn parse_byte(x: &str) -> u8 {
         u8::from_str_radix(x, 2).unwrap()
     }
-    fn make_len(mut v: Vec<u8>, l: usize) -> Vec<u8> {
+    fn make_len(mut v: Vec<u8>, l: usize) -> Vec<u8> { //code for formatting program
         if {v.len() == l} {v}
         else if {v.len() > l} {v[0..l].to_vec()}
         else {
@@ -27,18 +28,74 @@ fn run(program: Vec<String>) {
             v
         }
     }
-    fn format(bytes: Vec<u8>) -> [u8; 256] {
+    fn format(bytes: Vec<u8>) -> [u8; 256] { //more code for formatting raw program
         let mut array = [0; 256];
         array.copy_from_slice(&bytes[..256]);
         array
     }
-    fn load_disc(program: String, mut vm: Machine, dsc: u8) -> Result<(), &'static str> {
-        if {dsc as usize >= DISCS} {
-            return Err("Failed to set memory: Disc does not exist.");
+    for (i, v) in program.iter().enumerate() {
+        vm.loaddsc(i as u8, format(make_len(v.split(" ").map(parse_byte).collect(), 256)))?;
+        if {i > 2} {
+            break
         }
-         vm.mem[dsc as usize] = format(make_len(program.split(" ").map(parse_byte).collect(), 256));
-         Ok(())
     }
+    while {vm.isactive} {
+        let ins: [u8; 3] = [vm.mem(vm.dsccount, vm.prgcount)?, vm.mem(vm.dsccount, vm.prgcount+1)?, vm.mem(vm.dsccount, vm.prgcount+2)?];
+        match ins[0] {
+            0b00000000 => vm.end_END()?,
+            0b00001000 ... 0b00001111 => vm.ram_SAV_DIV(ins[1], ins[2], ins[0]&0b00000111)?,
+            0b00010000 ... 0b00010111 => vm.ram_SAV_DIR(ins[1], ins[2], ins[0]&0b00000111)?,
+            0b00011000 ... 0b00011111 => vm.ram_SAV_DRV(ins[1], ins[2], ins[0]&0b00000111)?,
+            0b00100000 ... 0b00100111 => vm.ram_SAV_DRR(ins[1], ins[2], ins[0]&0b00000111)?,
+            0b00101000 ... 0b00101111 => vm.ram_SAV_RIV(ins[1], ins[2], ins[0]&0b00000111)?,
+            0b00110000 ... 0b00110111 => vm.ram_SAV_RIR(ins[1], ins[2], ins[0]&0b00000111)?,
+            0b00111000 ... 0b00111111 => vm.ram_SAV_RRV(ins[1], ins[2], ins[0]&0b00000111)?,
+            0b01000000 ... 0b01000111 => vm.ram_SAV_RRR(ins[1], ins[2], ins[0]&0b00000111)?,
+            0b01001000 ... 0b01001111 => vm.ram_LOAD_DIR(ins[1], ins[2], ins[0]&0b00000111)?,
+            0b01010000 ... 0b01010111 => vm.ram_LOAD_DRR(ins[1], ins[2], ins[0]&0b00000111)?,
+            0b01011000 ... 0b01011111 => vm.ram_LOAD_RIR(ins[1], ins[2], ins[0]&0b00000111)?,
+            0b01100000 ... 0b01100111 => vm.ram_LOAD_DIR(ins[1], ins[2], ins[0]&0b00000111)?,
+            0b01101000 => vm.alu_SUB_RV(ins[1], ins[2])?,
+            0b01101001 => vm.alu_SUB_VR(ins[1], ins[2])?,
+            0b01101010 => vm.alu_SUB_RR(ins[1], ins[2])?,
+            0b01101011 => vm.alu_ADD_RV(ins[1], ins[2])?,
+            0b01101100 => vm.alu_ADD_RR(ins[1], ins[2])?,
+            0b01101101 => vm.alu_OR_RV(ins[1], ins[2])?,
+            0b01101110 => vm.alu_OR_RR(ins[1], ins[2])?,
+            0b01101111 => vm.alu_XOR_RV(ins[1], ins[2])?,
+            0b01110000 => vm.alu_XOR_RR(ins[1], ins[2])?,
+            0b01110001 => vm.alu_NOR_RV(ins[1], ins[2])?,
+            0b01110010 => vm.alu_NOR_RR(ins[1], ins[2])?,
+            0b01110011 => vm.alu_AND_RV(ins[1], ins[2])?,
+            0b01110100 => vm.alu_AND_RR(ins[1], ins[2])?,
+            0b01110101 => vm.cmp_GRT_RV(ins[1], ins[2])?,
+            0b01110110 => vm.cmp_GRT_RR(ins[1], ins[2])?,
+            0b01110111 => vm.cmp_LST_RV(ins[1], ins[2])?,
+            0b01111000 => vm.cmp_LST_RR(ins[1], ins[2])?,
+            0b01111001 => vm.cmp_GREQT_RV(ins[1], ins[2])?,
+            0b01111010 => vm.cmp_GREQT_RR(ins[1], ins[2])?,
+            0b01111011 => vm.cmp_LSEQT_RV(ins[1], ins[2])?,
+            0b01111100 => vm.cmp_LSEQT_RR(ins[1], ins[2])?,
+            0b01111101 => vm.cmp_EQL_RV(ins[1], ins[2])?,
+            0b01111110 => vm.cmp_EQL_RR(ins[1], ins[2])?,
+            0b01111111 => vm.cmp_NEQL_RV(ins[1], ins[2])?,
+            0b10000000 => vm.cmp_NEQL_RR(ins[1], ins[2])?,
+            0b10000001 => vm.goto_UNCON_DI(ins[1], ins[2])?,
+            0b10000010 => vm.goto_UNCON_DR(ins[1], ins[2])?,
+            0b10000011 => vm.goto_UNCON_RI(ins[1], ins[2])?,
+            0b10000100 => vm.goto_UNCON_RR(ins[1], ins[2])?,
+            0b10001000 ... 0b10001111 => vm.goto_ZRO_DRI(ins[1], ins[2], ins[0]&0b00000111)?,
+            0b10010000 ... 0b10010111 => vm.goto_ZRO_DRR(ins[1], ins[2], ins[0]&0b00000111)?,
+            0b10011000 ... 0b10011111 => vm.goto_ZRO_RRI(ins[1], ins[2], ins[0]&0b00000111)?,
+            0b10100000 ... 0b10100111 => vm.goto_ZRO_RRR(ins[1], ins[2], ins[0]&0b00000111)?,
+            0b10101000 ... 0b10001111 => vm.goto_NZRO_DRI(ins[1], ins[2], ins[0]&0b00000111)?,
+            0b10110000 ... 0b10010111 => vm.goto_NZRO_DRR(ins[1], ins[2], ins[0]&0b00000111)?,
+            0b10111000 ... 0b10011111 => vm.goto_NZRO_RRI(ins[1], ins[2], ins[0]&0b00000111)?,
+            0b11000000 ... 0b10100111 => vm.goto_NZRO_RRR(ins[1], ins[2], ins[0]&0b00000111)?,
+            _ => vm.end_END()?
+        }
+    }
+    Ok(())
 }
 
 impl Machine {
@@ -255,6 +312,11 @@ impl Machine {
         self.goto(self.reg(r1)?, self.reg(r2)?)
     }
 
+    fn end_END(&mut self) -> Result<(), &'static str> {
+        self.isactive = false;
+        Ok(())
+    }
+
     //non-instructions
     fn mem(&self, d: u8, i: u8) -> Result<u8, &'static str> { //return val at index
         let d = self.getdsc(d)? as usize;
@@ -304,6 +366,11 @@ impl Machine {
 
     fn getdsc(&self, d: u8) -> Result<u8, &'static str> { //get disc
         if {d==0} {Ok(self.dsccount)} else {Ok(d-1)}
+    }
+
+    fn loaddsc(&mut self, d: u8, f: [u8; 256]) -> Result<(), &'static str> {
+        self.mem[self.getdsc(d)? as usize] = f;
+        Ok(())
     }
 
 }
